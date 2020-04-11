239d7daf292cce8ac56678c57e90262e6b369226
PseudoResonance <kaio11602@gmail.com>
2020-04-11 00:49:37 -0700

    Update 1.0.4
    Use only necessary Data Dragon assets
    Switch to using Orianna API

--- src/main/java/com/github/pseudoresonance/resonantbot/leagueoflegends/LeagueOfLegendsCommand.java
+++ src/main/java/com/github/pseudoresonance/resonantbot/leagueoflegends/LeagueOfLegendsCommand.java
@@ -5,9 +5,6 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -21,7 +18,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
@@ -37,24 +33,31 @@ import com.github.pseudoresonance.resonantbot.data.Data;
 import com.github.pseudoresonance.resonantbot.language.Language;
 import com.github.pseudoresonance.resonantbot.language.LanguageManager;
 import com.github.pseudoresonance.resonantbot.permissions.PermissionGroup;
+import com.merakianalytics.orianna.Orianna;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.ForbiddenException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.InternalServerErrorException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.NotFoundException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.RateLimitExceededException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.ServiceUnavailableException;
+import com.merakianalytics.orianna.datapipeline.riotapi.exceptions.UnauthorizedException;
+import com.merakianalytics.orianna.types.common.Division;
+import com.merakianalytics.orianna.types.common.Platform;
+import com.merakianalytics.orianna.types.common.Region;
+import com.merakianalytics.orianna.types.common.Tier;
+import com.merakianalytics.orianna.types.core.league.LeagueEntry;
+import com.merakianalytics.orianna.types.core.league.LeaguePositions;
+import com.merakianalytics.orianna.types.core.staticdata.ProfileIcon;
+import com.merakianalytics.orianna.types.core.summoner.Summoner;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.rithms.riot.api.ApiConfig;
-import net.rithms.riot.api.RiotApi;
-import net.rithms.riot.api.RiotApiException;
-import net.rithms.riot.api.endpoints.league.dto.LeagueEntry;
-import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
-import net.rithms.riot.constant.Platform;
 
 public class LeagueOfLegendsCommand {
 
 	private static final long SUMMONER_EXPIRY = 900000;
 
 	private static CommandHandler cmd = null;
-	private static ApiConfig config = null;
-	private static RiotApi api = null;
 
 	private static String apiKey = "";
 
@@ -65,26 +68,23 @@ public class LeagueOfLegendsCommand {
 	private static ConcurrentHashMap<String, ExpiryHolder<Summoner>> summonerAccountIdCache = new ConcurrentHashMap<String, ExpiryHolder<Summoner>>();
 	private static ConcurrentHashMap<String, ExpiryHolder<Summoner>> summonerPuuidCache = new ConcurrentHashMap<String, ExpiryHolder<Summoner>>();
 
-	private static ConcurrentHashMap<String, ExpiryHolder<Set<LeagueEntry>>> summonerLeagueEntriesCache = new ConcurrentHashMap<String, ExpiryHolder<Set<LeagueEntry>>>();
+	private static ConcurrentHashMap<String, ExpiryHolder<LeaguePositions>> summonerLeagueEntriesCache = new ConcurrentHashMap<String, ExpiryHolder<LeaguePositions>>();
 
 	private static ConcurrentHashMap<String, String> summonerPictureCache = new ConcurrentHashMap<String, String>();
 
 	public static void setup(Plugin plugin) {
-		if (api == null) {
-			Object keyO = Data.getBotSetting("leagueoflegends_key");
-			if (keyO instanceof String) {
-				String key = (String) keyO;
-				apiKey = key;
-				config = new ApiConfig().setKey(key);
-				api = new RiotApi(config);
-			}
+		Object keyO = Data.getBotSetting("leagueoflegends_key");
+		if (keyO instanceof String) {
+			String key = (String) keyO;
+			apiKey = key;
+			Orianna.setRiotAPIKey(key);
 		}
 		cmd = new CommandHandler("leagueoflegends", "leagueoflegends.lolCommandDescription");
 		cmd.registerSubcommand("apikey", (e, command, args) -> {
 			if (args.length > 0) {
 				e.getMessage().delete().queue();
 				Data.setBotSetting("leagueoflegends_key", args[0]);
-				config.setKey(args[0]);
+				Orianna.setRiotAPIKey(args[0]);
 				apiKey = args[0];
 				e.getChannel().sendMessage(LanguageManager.getLanguage(e).getMessage("main.savedAPIKey")).queue();
 			} else {
@@ -138,20 +138,14 @@ public class LeagueOfLegendsCommand {
 						ExpiryHolder<Summoner> cachedSummoner = checkSummonerCache(name, platform);
 						Summoner summoner = null;
 						if (cachedSummoner == null || cachedSummoner.isExpired()) {
-							if (name.length() <= 16) {
-								summoner = api.getSummonerByName(platform, name);
-							} else if (name.length() == 78) {
-								summoner = api.getSummonerByPuuid(platform, name);
-							} else {
-								summoner = api.getSummonerByAccount(platform, name);
-							}
+							summoner = Orianna.summonerNamed(name).withPlatform(platform).get();
 							cachedSummoner = updateSummonerCache(summoner, platform);
 						} else
 							summoner = cachedSummoner.getObject();
-						ExpiryHolder<Set<LeagueEntry>> cachedLeagueEntries = checkSummonerLeagueEntriesCache(summoner.getPuuid());
-						Set<LeagueEntry> leagueEntries = null;
+						ExpiryHolder<LeaguePositions> cachedLeagueEntries = checkSummonerLeagueEntriesCache(summoner.getPuuid());
+						LeaguePositions leagueEntries = null;
 						if (cachedLeagueEntries == null || cachedLeagueEntries.isExpired()) {
-							leagueEntries = api.getLeagueEntriesBySummonerId(platform, summoner.getId());
+							leagueEntries = Orianna.leaguePositionsForSummoner(summoner).get();
 							cachedLeagueEntries = updateSummonerLeagueEntriesCache(summoner.getPuuid(), cachedSummoner.getCreation(), leagueEntries);
 						}
 						try {
@@ -175,12 +169,16 @@ public class LeagueOfLegendsCommand {
 						} catch (InterruptedException | ExecutionException e2) {
 							e.getChannel().sendMessage(text).queue();
 						}
-					} catch (RiotApiException e1) {
+					} catch (Exception e1) {
 						String text = "";
-						if (e1.getErrorCode() == 403)
+						if (e1 instanceof UnauthorizedException || e1 instanceof ForbiddenException)
 							text = LanguageManager.getLanguage(e).getMessage("main.noAPIKey");
-						else if (e1.getErrorCode() == 404)
+						else if (e1 instanceof NotFoundException)
 							text = LanguageManager.getLanguage(e).getMessage("leagueoflegends.invalidSummoner");
+						else if (e1 instanceof RateLimitExceededException)
+							text = LanguageManager.getLanguage(e).getMessage("main.rateLimit");
+						else if (e1 instanceof InternalServerErrorException || e1 instanceof ServiceUnavailableException)
+							text = LanguageManager.getLanguage(e).getMessage("main.serviceError");
 						else {
 							e1.printStackTrace();
 							text = LanguageManager.getLanguage(e).getMessage("main.errorOccurred");
@@ -198,7 +196,7 @@ public class LeagueOfLegendsCommand {
 			if (showPlatforms) {
 				String options = "";
 				for (Platform platform : Platform.values())
-					options += "`" + platform.getName().toLowerCase() + "`, ";
+					options += "`" + platform.getTag().toLowerCase() + "`, ";
 				options = options.substring(0, options.length() - 1);
 				e.getChannel().sendMessage(LanguageManager.getLanguage(e).getMessage("main.validSubcommands", options)).queue();
 				return true;
@@ -211,7 +209,7 @@ public class LeagueOfLegendsCommand {
 	private static ExpiryHolder<Summoner> checkSummonerCache(String input, Platform platform) {
 		input = input.toLowerCase();
 		if (input.length() <= 16) {
-			return summonerNameCache.get(input + "#" + platform.getId());
+			return summonerNameCache.get(input + "#" + platform.getTag().toUpperCase());
 		} else if (input.length() == 78) {
 			return summonerPuuidCache.get(input);
 		} else {
@@ -222,7 +220,7 @@ public class LeagueOfLegendsCommand {
 		}
 	}
 
-	private static ExpiryHolder<Set<LeagueEntry>> checkSummonerLeagueEntriesCache(String puuid) {
+	private static ExpiryHolder<LeaguePositions> checkSummonerLeagueEntriesCache(String puuid) {
 		puuid = puuid.toLowerCase();
 		return summonerLeagueEntriesCache.get(puuid);
 	}
@@ -236,15 +234,15 @@ public class LeagueOfLegendsCommand {
 				summonerNameCache.remove(oldName);
 		}
 		ExpiryHolder<Summoner> expiry = new ExpiryHolder<Summoner>(summoner, SUMMONER_EXPIRY);
-		summonerNameCache.put(newName + "#" + platform.getId(), expiry);
+		summonerNameCache.put(newName + "#" + platform.getTag().toUpperCase(), expiry);
 		summonerIdCache.put(summoner.getId().toLowerCase(), expiry);
 		summonerAccountIdCache.put(summoner.getAccountId().toLowerCase(), expiry);
 		summonerPuuidCache.put(summoner.getPuuid().toLowerCase(), expiry);
 		return expiry;
 	}
 
-	private static ExpiryHolder<Set<LeagueEntry>> updateSummonerLeagueEntriesCache(String puuid, long creation, Set<LeagueEntry> entries) {
-		ExpiryHolder<Set<LeagueEntry>> expiry = new ExpiryHolder<Set<LeagueEntry>>(entries, SUMMONER_EXPIRY);
+	private static ExpiryHolder<LeaguePositions> updateSummonerLeagueEntriesCache(String puuid, long creation, LeaguePositions entries) {
+		ExpiryHolder<LeaguePositions> expiry = new ExpiryHolder<LeaguePositions>(entries, SUMMONER_EXPIRY);
 		expiry.setExpiry(creation, SUMMONER_EXPIRY);
 		summonerLeagueEntriesCache.put(puuid.toLowerCase(), expiry);
 		return expiry;
@@ -252,41 +250,46 @@ public class LeagueOfLegendsCommand {
 
 	private static Platform getPlatformByName(String name) {
 		for (Platform platform : Platform.values()) {
-			if (platform.getName().equalsIgnoreCase(name) || platform.getId().equalsIgnoreCase(name)) {
+			if (platform.getTag().equalsIgnoreCase(name)) {
 				return platform;
+			}
+		}
+		for (Region region : Region.values()) {
+			if (region.getTag().equalsIgnoreCase(name)) {
+				return region.getPlatform();
 			}
 		}
 		throw new NoSuchElementException("Unknown platform name: " + name);
 	}
 
-	private static void sendSummonerInformation(MessageReceivedEvent e, ExpiryHolder<Summoner> summonerHolder, Platform platform, ExpiryHolder<Set<LeagueEntry>> leagueEntries, CompletableFuture<Message> placeholder) {
+	private static void sendSummonerInformation(MessageReceivedEvent e, ExpiryHolder<Summoner> summonerHolder, Platform platform, ExpiryHolder<LeaguePositions> leagueEntries, CompletableFuture<Message> placeholder) {
 		try {
 			Summoner summoner = summonerHolder.getObject();
-			Set<LeagueEntry> leagues = leagueEntries.getObject();
+			LeaguePositions leagues = leagueEntries.getObject();
 			Language lang = LanguageManager.getLanguage(e);
 			EmbedBuilder embed = new EmbedBuilder();
 			embed.setFooter(lang.formatTimeAgo(new Timestamp(summonerHolder.getCreation())));
 			embed.setColor(new Color(224, 201, 121));
-			embed.setTitle(lang.getMessage("leagueoflegends.summonerinfo", LanguageManager.escape(summoner.getName() + "#" + platform.getId()), summoner.getSummonerLevel()), "https://" + platform.getName() + ".op.gg/summoner/userName=" + URLEncoder.encode(summoner.getName(), "UTF-8"));
+			embed.setTitle(lang.getMessage("leagueoflegends.summonerinfo", LanguageManager.escape(summoner.getName() + "#" + platform.getTag().toUpperCase()), summoner.getLevel()), "https://" + platform.getRegion().getTag() + ".op.gg/summoner/userName=" + URLEncoder.encode(summoner.getName(), "UTF-8"));
 			embed.setThumbnail("attachment://thumb.png");
-			LeagueTier highestTier = null;
-			String tierRank = "";
+			Tier highestTier = null;
+			Division highestDivision = null;
 			for (LeagueEntry entry : leagues) {
-				LeagueTier tier = LeagueTier.valueOf(entry.getTier().toUpperCase());
+				Tier tier = entry.getTier();
 				if (tier != null) {
-					if (highestTier == null || tier.getId() < highestTier.getId()) {
+					if (highestTier == null || highestTier.compare(tier) < 0) {
 						highestTier = tier;
-						tierRank = entry.getRank();
+						highestDivision = entry.getDivision();
 					}
 				}
-				String entryStats = lang.getMessage("leagueoflegends.tier", WordUtils.capitalizeFully(tier.toString()) + (tier != LeagueTier.CHALLENGER && tier != LeagueTier.GRANDMASTER && tier != LeagueTier.MASTER ? " " + entry.getRank().toUpperCase() : ""));
+				String entryStats = lang.getMessage("leagueoflegends.tier", WordUtils.capitalizeFully(tier.toString()) + (tier != Tier.CHALLENGER && tier != Tier.GRANDMASTER && tier != Tier.MASTER ? " " + entry.getDivision().toString().toUpperCase() : ""));
 				entryStats += "\n" + lang.getMessage("leagueoflegends.winlossratio", entry.getWins(), entry.getLosses(), df.format((entry.getWins() / (double) (entry.getWins() + entry.getLosses()) * 100)));
 				entryStats += "\n" + entry.getLeaguePoints() + " LP" + (entry.isVeteran() ? " " + lang.getMessage("leagueoflegends.veteran") : "");
-				if (entry.isFreshBlood() || entry.isHotStreak() || entry.isInactive())
-					entryStats += "\n" + (entry.isInactive() ? " " + lang.getMessage("leagueoflegends.inactive") : "") + (entry.isFreshBlood() ? " " + lang.getMessage("leagueoflegends.freshBlood") : "") + (entry.isHotStreak() ? " " + lang.getMessage("leagueoflegends.hotStreak") : "");
-				embed.addField(WordUtils.capitalizeFully(entry.getQueueType().replace('_', ' ')), entryStats, true);
+				if (entry.isFreshBlood() || entry.isOnHotStreak() || entry.isInactive())
+					entryStats += "\n" + (entry.isInactive() ? " " + lang.getMessage("leagueoflegends.inactive") : "") + (entry.isFreshBlood() ? " " + lang.getMessage("leagueoflegends.freshBlood") : "") + (entry.isOnHotStreak() ? " " + lang.getMessage("leagueoflegends.hotStreak") : "");
+				embed.addField(WordUtils.capitalizeFully(entry.getQueue().getTag().replace('_', ' ')), entryStats, true);
 			}
-			LocalDateTime revisionDate = LocalDateTime.ofInstant(Instant.ofEpochMilli(summoner.getRevisionDate()), ZoneId.systemDefault());
+			LocalDateTime revisionDate = LocalDateTime.ofInstant(Instant.ofEpochMilli(summoner.getUpdated().getMillis()), ZoneId.systemDefault());
 			long daysSinceRevision = ChronoUnit.DAYS.between(revisionDate, LocalDateTime.now());
 			String description = "";
 			if (daysSinceRevision >= 1)
@@ -294,10 +297,9 @@ public class LeagueOfLegendsCommand {
 			else
 				description += lang.formatTimeAgo(revisionDate, false);
 			description = lang.getMessage("leagueoflegends.updated", lang.getMessage("date.relativeAgo", description));
-			description = "`" + summoner.getAccountId() + "`\n" + description;
-			description += "\n" + lang.getMessage("leagueoflegends.tier", highestTier == null ? lang.getMessage("leagueoflegends.unranked") : WordUtils.capitalizeFully(highestTier.toString()) + (highestTier != LeagueTier.CHALLENGER && highestTier != LeagueTier.GRANDMASTER && highestTier != LeagueTier.MASTER ? " " + tierRank.toUpperCase() : ""));
+			description += "\n" + lang.getMessage("leagueoflegends.tier", highestTier == null ? lang.getMessage("leagueoflegends.unranked") : WordUtils.capitalizeFully(highestTier.toString()) + (highestTier != Tier.CHALLENGER && highestTier != Tier.GRANDMASTER && highestTier != Tier.MASTER ? " " + highestDivision.toString().toUpperCase() : ""));
 			embed.setDescription(description);
-			String url = getProfilePictureCache(summoner.getProfileIconId(), highestTier);
+			String url = getProfilePictureCache(summoner.getProfileIcon(), highestTier);
 			if (url != null) {
 				embed.setThumbnail(url);
 				try {
@@ -309,7 +311,7 @@ public class LeagueOfLegendsCommand {
 			} else {
 				InputStream thumb = getProfilePicture(summoner, highestTier);
 				if (thumb == null) {
-					embed.setThumbnail("https://opgg-static.akamaized.net/images/profile_icons/profileIcon" + summoner.getProfileIconId() + ".jpg");
+					embed.setThumbnail(summoner.getProfileIcon().getImage().getURL());
 					try {
 						Message msg = placeholder.get();
 						msg.editMessage(embed.build()).override(true).queue();
@@ -323,7 +325,8 @@ public class LeagueOfLegendsCommand {
 					} catch (NullPointerException | InterruptedException | ExecutionException e1) {
 					}
 					Message sent = e.getChannel().sendMessage(embed.build()).addFile(thumb, "thumb.png").complete();
-					summonerPictureCache.put((highestTier == null ? "" : highestTier.toString().toLowerCase()) + summoner.getProfileIconId(), sent.getAttachments().get(0).getUrl());
+					if (sent != null && sent.getAttachments().size() > 0)
+						summonerPictureCache.put((highestTier == null ? "" : highestTier.toString().toLowerCase()) + summoner.getProfileIcon().getId(), sent.getAttachments().get(0).getUrl());
 				}
 				if (thumb != null)
 					thumb.close();
@@ -334,35 +337,29 @@ public class LeagueOfLegendsCommand {
 		}
 	}
 
-	private static String getProfilePictureCache(int pictureId, LeagueTier tier) {
-		return summonerPictureCache.get((tier == null ? "" : tier.toString().toLowerCase()) + pictureId);
+	private static String getProfilePictureCache(ProfileIcon picture, Tier tier) {
+		return summonerPictureCache.get((tier == null ? "" : tier.toString().toLowerCase()) + picture.getId());
 	}
 
-	private static InputStream getProfilePicture(Summoner summoner, LeagueTier tier) {
-		File iconFile = new File(LeagueOfLegends.getPatchDataDirectory(), LeagueOfLegends.getPatchName() + File.separator + "img" + File.separator + "profileicon" + File.separator + summoner.getProfileIconId() + ".png");
-		if (tier == null) {
-			try {
-				return new FileInputStream(iconFile);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
+	private static InputStream getProfilePicture(Summoner summoner, Tier tier) {
 		try {
-			BufferedImage icon = ImageIO.read(iconFile);
-			BufferedImage border = ImageIO.read(new URL("https://opgg-static.akamaized.net/images/borders2/" + tier.toString().toLowerCase() + ".png"));
-			BufferedImage combined = new BufferedImage(120, 120, BufferedImage.TYPE_INT_ARGB);
-			Graphics2D g = combined.createGraphics();
-			g.drawImage(icon, 10, 10, 100, 100, null);
-			g.drawImage(border, 0, 0, null);
-			final ByteArrayOutputStream output = new ByteArrayOutputStream() {
-				@Override
-				public synchronized byte[] toByteArray() {
-					return this.buf;
-				}
-			};
-			ImageIO.write(combined, "png", output);
-			return new ByteArrayInputStream(output.toByteArray(), 0, output.size());
+			BufferedImage icon = ImageIO.read(new URL(summoner.getProfileIcon().getImage().getURL()));
+			if (tier != null) {
+				BufferedImage border = ImageIO.read(new URL("https://opgg-static.akamaized.net/images/borders2/" + tier.toString().toLowerCase() + ".png"));
+				BufferedImage combined = new BufferedImage(120, 120, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D g = combined.createGraphics();
+				g.drawImage(icon, 10, 10, 100, 100, null);
+				g.drawImage(border, 0, 0, null);
+				final ByteArrayOutputStream output = new ByteArrayOutputStream() {
+					@Override
+					public synchronized byte[] toByteArray() {
+						return this.buf;
+					}
+				};
+				ImageIO.write(combined, "png", output);
+				return new ByteArrayInputStream(output.toByteArray(), 0, output.size());
+			}
+			return null;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
@@ -381,20 +378,6 @@ public class LeagueOfLegendsCommand {
 				summonerLeagueEntriesCache.remove(s.getPuuid());
 				iter.remove();
 			}
-		}
-	}
-
-	private static enum LeagueTier {
-		CHALLENGER(1), GRANDMASTER(2), MASTER(3), DIAMOND(4), PLATINUM(5), GOLD(6), SILVER(7), BRONZE(8), IRON(9);
-
-		int id = 0;
-
-		LeagueTier(int id) {
-			this.id = id;
-		}
-
-		public int getId() {
-			return id;
 		}
 	}
 
